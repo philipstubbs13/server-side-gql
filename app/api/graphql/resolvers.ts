d60945c45f3e71581fe3b1f5ab46755257a028ce
Phil Stubbs <philipstubbs13@gmail.com>
2024-07-06 18:43:51 -0500

    graphql

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,3 +1,34 @@
-const resolvers = {}
+const resolvers = {
+    SearchType: {
+        __resolveType: (obj) => {
+            if (obj.species) {
+                return 'Animal'
+            }
+            return 'Person'
+        },
+    },
+    Person: {
+      name: (person) => {
+        return person.name.toUpperCase();
+      },
+      pets: (person) => {
+        return [{species: 'cat', name: 'darrylk'}]
+      }
+    },
+    Query: {
+        search: () => {
+            return [
+                { name: 'Scott', id: 'fkdajflkdjf'},
+                { name: 'Darryl', species: 'Big cat' }
+            ]
+        },
+        me: () => {
+            return 'me'
+        },
+        people: () => {
+            return [{ id: 'sajkdlfj', name: 'henry', pets: [1] }]
+        }
+    }
+}
 
 export default resolvers
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,5 +1,29 @@
 const schema = `#graphql
+    type Animal {
+        species: String!
+        name: String!
+    }
 
+    enum COLORS {
+        BLUE
+        PURPLE
+        BROWN
+    }
+
+    type Person {
+        name: String!
+        id: ID!
+        faveColor: COLORS!
+        pets: [Animal]!
+    }
+
+    union SearchType = Animal | Person
+
+    type Query {
+        me: String!
+        people: [Person!]!
+        search: [SearchType]! 
+    }
 `
 
 export default schema
